50980aadc6038e4fb1e602366955461f7cd4846c
tom <aa123593465@gmail.com>
2024-03-25 05:29:01 +0000

    fix: getdents64

--- hw2/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/tomlord/sys/hw2/modules.order := {   echo /home/tomlord/sys/hw2/rootkit.ko; :; } | awk '!x[$$0]++' - > /home/tomlord/sys/hw2/modules.order
--- hw2/.rootkit.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/tomlord/sys/hw2/rootkit.mod := { echo  /home/tomlord/sys/hw2/rootkit.o;  echo; } > /home/tomlord/sys/hw2/rootkit.mod
