4913fe2a872287a47b155f685fa0a1a181dc1908
tomlord <aa123593465@gmail.com>
2024-06-01 12:39:12 +0800

    chore: hw4 stuff

---
+++ hw4/passes/hw4.cpp
@@ -0,0 +1,55 @@
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constant.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/DebugLoc.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+using namespace std;
+
+namespace {
+struct VTableCheckerModulePass
+    : public PassInfoMixin<VTableCheckerModulePass> {
+private:
+  bool runOnModule(Module &M);
+
+public:
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    runOnModule(M);
+    return PreservedAnalyses::all();
+  }
+};
+} // namespace llvm
+
+bool VTableCheckerModulePass::runOnModule(Module &M) {
+	outs() << __FUNCTION__ << '\n';
+	// do something
+  return true;
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {.APIVersion = LLVM_PLUGIN_API_VERSION,
+          .PluginName = "VTable checker pass",
+          .PluginVersion = "v0.1",
+          .RegisterPassBuilderCallbacks =
+              [](PassBuilder &PB) {
+                PB.registerPipelineStartEPCallback(
+                    [](ModulePassManager &MPM, OptimizationLevel Level) {
+                      MPM.addPass(VTableCheckerModulePass());
+                    });
+              }
+
+  };
+}
